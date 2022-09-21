4c51bc79a197bf269100446692a25b29acf0a6df
Franklyn R <franklyn1998@live.com>
2022-09-21 18:52:21 -0300

    Criando interface de autenticação

---
+++ app/src/main/java/com/hadesfranklyn/uber/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hadesfranklyn.uber.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.hadesfranklyn.uber.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/hadesfranklyn/uber/activity/MapsActivity.java
+++ app/src/main/java/com/hadesfranklyn/uber/activity/MapsActivity.java
@@ -1,8 +1,8 @@
-package com.hadesfranklyn.uber;
-
-import androidx.fragment.app.FragmentActivity;
+package com.hadesfranklyn.uber.activity;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,23 +10,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.ktx.Firebase;
-import com.hadesfranklyn.uber.databinding.ActivityMapsBinding;
+import com.hadesfranklyn.uber.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
