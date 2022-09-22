d4574f61d3c0cb00fbc6b51aae89c5a367cb0c71
Franklyn R <franklyn1998@live.com>
2022-09-22 19:26:53 -0300

    Configurando mapas & permissões

--- app/src/main/java/com/hadesfranklyn/uber/activity/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/uber/activity/MainActivity.java
@@ -1,31 +1,44 @@
 package com.hadesfranklyn.uber.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.hadesfranklyn.uber.R;
+import com.hadesfranklyn.uber.config.ConfiguracaoFirebase;
+import com.hadesfranklyn.uber.helper.Permissoes;
 import com.hadesfranklyn.uber.helper.UsuarioFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonEntrar;
-    private Button buttonCadastrar;
+    private FirebaseAuth autenticacao;
+    private String[] permissoes = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializar componentes
-        buttonEntrar = findViewById(R.id.buttonEntrar);
-        buttonCadastrar = findViewById(R.id.buttonCadastrar);
-
         // Esconde a toolbar
         getSupportActionBar().hide();
+
+        //validar permissoes
+        Permissoes.validarPermissoes(permissoes, this , 1);
+
+//        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+//        autenticacao.signOut();
+
     }
 
 
@@ -41,5 +54,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         UsuarioFirebase.redirecionaUsuarioLogado(MainActivity.this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões!");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
